40f44edb119978797ced9ce4977b5eabd05c1876
Mirza Cevanşir Gür <mirzacevansirgur@Mirza-MacBook-Pro.local>
2016-10-30 22:35:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi1[] = new int[5];
+        //Dizileri bu sekildede bildirim yapip tanimlayabiliriz.
+        int []araba = new int[5];
+        char karakter[][]= new  char[7][8];
+        float [] sayisal,abc,xyz; //3 tane dizi tanimlandi.
+        double insan[], hayvan[]; //2 tane dizi tanimlandi.
+    }
+}
